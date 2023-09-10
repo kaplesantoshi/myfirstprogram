7cea11aed28a8b4e2b6f6105d570f058f07904ce
kaplesantoshi <144530685+kaplesantoshi@users.noreply.github.com>
2023-09-10 14:37:10 +0530

    Add files via upload

---
+++ myfirstjavapro.java
@@ -0,0 +1,18 @@
+
+public class myfirstjavapro {
+	
+	public static void main(String[] args) {
+		System.out.println("Integer bits; " + Integer.SIZE);
+		System.out.println("Integer bytes: " + Integer.SIZE/8);
+		System.out.println("Integer max range; " + Integer.MAX_VALUE);
+		System.out.println("Integer min range; "+ Integer.MIN_VALUE);
+		
+		
+		
+		
+		System.out.println("Double bits; " + Double.SIZE);
+		System.out.println("Double bytes: " + Double.SIZE/8);
+		System.out.println(" Double max range; " + Double.MAX_VALUE);
+		System.out.println("Double min range; "+ Double.MIN_VALUE);
+	}
+}
